584bcd4c087b53a35e7371eeca0ee45447340077
Harsh <hardhgondaliyo@gmail.com>
2026-05-14 00:36:46 +0530

    fix: remove spaces from email password

--- Loaferss-backend/controllers/authController.js
+++ Loaferss-backend/controllers/authController.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
     maxMessages: 100,
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
+        pass: process.env.EMAIL_PASS ? process.env.EMAIL_PASS.replace(/\s+/g, '') : ''
     }
 });
 
